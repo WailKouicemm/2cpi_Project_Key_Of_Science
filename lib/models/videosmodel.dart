6304268ded0246417d63ab43a283ca9db207c548
MouadBaghdadi <mouadbaghdadi2002@gmail.com>
2022-03-14 00:24:10 +0100

    add of 3 pages

---
+++ lib/models/videosmodel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class VideoModel{
+  final String title;
+  final Widget iconn;
+  VideoModel({
+    required this.iconn,
+    required this.title,
+  });
+}
