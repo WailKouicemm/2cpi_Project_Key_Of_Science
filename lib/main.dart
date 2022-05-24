e4635edabff7084568a64c0930fb7738b470756d
salah <salaheddinesalhi4@gmail.com>
2022-05-24 20:15:32 +0200

    7 th+++

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class MyApp extends StatelessWidget {
                 return const NextPage();
               }
               return const loginORregister();
-            },
+             },
           ),
         ));
   }
