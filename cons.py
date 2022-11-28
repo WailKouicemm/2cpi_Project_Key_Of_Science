a2be13a61388586b6acc0e390a9c01c1d98f874d
Baghdadi Mohammed Mouad <101417636+MouadBaghdadi@users.noreply.github.com>
2022-11-28 00:57:49 +0000

    azazaz

--- cons.py
+++ cons.py
@@ -2,4 +2,8 @@
 print('cons fork')
 print('cons fork')
 print('cons fork')
+def predict(predictors, weather_imp, reg):
+    train_set = weather_imp.loc[:'2015-01-01']
+    test_set = weather_imp.loc['2015-01-01':]
+    return r
 
